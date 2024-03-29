022fc4eae34d4fc979543c22f31613ec86472550
크로스벨 <crossbell@keuloseubel-ui-MacBookPro.local>
2024-03-29 22:24:32 +0900

    [feat] Add Basic Project * 프로젝트 기본설정 진행   - Java17, SpringBoot 3.2.4, Gradle-Groovy 기반   - 관련 gitignore 설정진행 * Dependency   - Lombok, SpringWeb, SpringSecurity, SpringDataJPA, PostgreSQL Driver, SpringBoot DevTools, SpringBoot Actuator, SpringBoot Configuration Processor * Properties 구분   - application, application-secret 파일구분 & gitignore 추가

---
+++ src/main/java/com/team1os/todopangAdmin/TodopangAdminApplication.java
@@ -0,0 +1,13 @@
+package com.team1os.todopangAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodopangAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodopangAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team1os/todopangAdmin/TodopangAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1os.todopangAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodopangAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
